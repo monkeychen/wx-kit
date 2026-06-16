4f4f2e7fb42009ce4c1c3c0cba29bfc2efbd0f66
chenzhian <cza55007@gmail.com>
2026-06-16 19:06:39 +0800

    feat(renderer): subscriptions page + route + nav item with new-article badge

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -4,6 +4,7 @@ import Download from './pages/Download'
 import Library from './pages/Library'
 import Reader from './pages/Reader'
 import Settings from './pages/Settings'
+import Subscriptions from './pages/Subscriptions'
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Route element={<MainLayout />}>
         <Route index element={<Download />} />
         <Route path="batch" element={<Navigate to="/" replace />} />
+        <Route path="subscriptions" element={<Subscriptions />} />
         <Route path="library" element={<Library />} />
         <Route path="reader/:id" element={<Reader />} />
         <Route path="settings" element={<Settings />} />
